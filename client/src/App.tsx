998b9b6da477cc6ada5e065b0aeaddfc71eeafb0
KhaledGharib <frkhwg@gmail.com>
2023-09-04 17:38:52 +0300

    upload

---
+++ client/src/App.tsx
@@ -0,0 +1,78 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { Input } from "@/components/ui/input";
+import { MapPinIcon, PaperAirplaneIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  const socket = io("http://localhost:4000");
+
+  const [message, setMessage] = useState<string>("");
+  const [sentMessages, setSentMessages] = useState<string[]>([]);
+
+  const handleSend = () => {
+    if (message.trim() !== "") {
+      socket.emit("sendMessage", message);
+      setMessage(""); // Clear the input field after sending
+    }
+  };
+
+  const handleLocation = () => {
+    if (!navigator.geolocation) {
+      return alert("Geolocation not supported");
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      socket.emit("sendLocation", {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  };
+  useEffect(() => {
+    socket.on("message", (message) => {
+      console.log(message);
+      setSentMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  return (
+    <div className="w-full min-h-screen bg-slate-500">
+      <div className="flex flex-col justify-between min-h-screen container">
+        <div className="flex flex-col gap-4 py-5">
+          {sentMessages.map((sentMessage, index) => (
+            <div className="flex gap-3" key={index}>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div
+                className="bg-gray-200 p-3 rounded-xl rounded-bl-none max-w-fit"
+                dangerouslySetInnerHTML={{ __html: sentMessage }}
+              ></div>
+            </div>
+          ))}
+        </div>
+        <form onClick={(e) => e.preventDefault()}>
+          <div className="flex py-5 gap-3">
+            <Input
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              placeholder="Write your message 💬"
+              type="text"
+              name="message"
+              id="message"
+            />
+            <Button onClick={handleSend}>
+              <PaperAirplaneIcon className="w-5 h-5" />
+            </Button>
+            <Button onClick={handleLocation}>
+              <MapPinIcon className="h-6 w-4" />
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const socketIo = require("socket.io");
+const http = require("http");
+const cors = require("cors"); // Import the cors middleware
+
+const app = express();
+const server = http.createServer(app);
+
+// Enable CORS for regular HTTP requests
+app.use(cors());
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const port = process.env.PORT || 4000;
+
+let count = 0;
+io.on("connection", (socket) => {
+  console.log("New user connected!");
+
+  //   socket.emit("message", "Welcome!");
+
+  // Listen for incoming messages from the client
+  socket.on("sendMessage", (message) => {
+    // Broadcast the message to all connected clients, including the sender
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", (coords) => {
+    const googleMapsLink = `Check my location 👇 <br>
+    <a href="https://www.google.com/maps?q=${coords.latitude},${coords.longitude}"  class="location-message" target="_blank">Location 📍</a>`;
+
+    io.emit("message", googleMapsLink);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected!");
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server running on port " + port);
+});
